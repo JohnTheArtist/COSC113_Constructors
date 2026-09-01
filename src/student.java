32ead6398729b666811d4468f2c528d80996af3f
JohnTheArtist <johnr2d20@gmail.com>
2026-09-01 10:41:55 -0400

    second commit

--- src/student.java
+++ src/student.java
@@ -17,6 +17,26 @@ public class student {
     // JVM access the static method using ClassName.MethodName
 
 
+    //constructors dont have any return type
+    //constructors must have the same name as the class name
+    //constructors are useful for initialization
+
+
+    student() {
+        FN = "John";
+        LN = "Jonas";
+        ID = 230;
+
+
+
+    }
+
+    student ( String FN, String LN, int ID ){
+        this.FN = FN;
+        this.LN = LN;
+        this.ID = ID;
+    }
+
     public static void main(String [] args){
 
 
@@ -24,8 +44,17 @@ public class student {
         student s1 = new student();
         System.out.println(s1);
 
+        student s2 = new student();
+
+        student s3 = new student("James" , "John", 211);
+
         // use of the dot operator "."
         System.out.println( "First name: " + s1.FN);
+
+        System.out.println( "First name: " + s2.FN);
+
+        System.out.println( "First name: " + s3.FN);
+
         System.out.println("Hello Constructors");
 
 
